8626b347f634cd7d8b4fc904c7267c6786f6479b
Chukwuka1488 <juniorakibor@yahoo.com>
2021-07-06 12:41:53 +0200

    clock component created

--- ClockApp/src/app/app-routing.module.ts
+++ ClockApp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClockComponent } from './clock/clock.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '**', component: ClockComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
